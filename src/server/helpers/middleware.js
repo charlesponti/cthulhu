f93a9d38aaad29a5e347f13bd2c91462d82c0f11
CJ Ponti <cjponti@gmail.com>
2014-10-14 22:34:07 +0100

    Add lusca.csrf() to CthulhuMiddleware as _csrf

--- src/server/helpers/middleware.js
+++ src/server/helpers/middleware.js
@@ -7,7 +7,6 @@
 var _ = require('lodash');
 var chalk = require('chalk');
 var lusca = require('lusca');
-var csrf = lusca.csrf();
 var User = require('../models/user');
 
 function CthulhuMiddleware() {
@@ -17,6 +16,12 @@ function CthulhuMiddleware() {
    * @type {Object}
    */
   var self = this;
+
+  /**
+   * Add default csrf function
+   * @type {Function}
+   */
+  self._csrf = lusca.csrf();
 
   /**
    * Server either the development version of the minified version of
@@ -172,7 +177,7 @@ function CthulhuMiddleware() {
         });
       }
     } else {
-      csrf(req, res, next);
+      self._csrf(req, res, next);
     }
   };
 
