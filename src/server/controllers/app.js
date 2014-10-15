d4bb1f6c69927667d151a642592372c411b936d9
CJ Ponti <cjponti@gmail.com>
2014-10-15 17:37:27 +0100

    Add default route to server layout

--- src/server/controllers/app.js
+++ src/server/controllers/app.js
@@ -5,8 +5,8 @@ var router = express.Router();
 
 /**
  * Render static/about.jade
- * @param  {http.IncomingMessage} req
- * @param  {http.ServerResponse} res
+ * @param  {IncomingMessage} req
+ * @param  {ServerResponse} res
  */
 router.get('/about', function(req, res) {
   res.render('static/about');
@@ -14,8 +14,8 @@ router.get('/about', function(req, res) {
 
 /**
  * Handler for errors
- * @param {http.IncomingMessage} req
- * @param {http.ServerResponse} res
+ * @param {IncomingMessage} req
+ * @param {ServerResponse} res
  * @param {Object} config
  *     @params {String} config.msg Message to display in flash message
  *     @params {String} config.url Url for redirect
@@ -23,19 +23,28 @@ router.get('/about', function(req, res) {
  * TODO Set up central error logging system
  */
 router.get('/error', function(err, req, res) {
-  var message = "There was an issue processing your request. Our unicorns will"+
-    " look into the issue after their trip to Candy Mountain.";
-  req.flash("error", err.message || message);
-  res.redirect("/");
+  var message = 'There was an issue processing your request. Our unicorns will'+
+    ' look into the issue after their trip to Candy Mountain.';
+  req.flash('error', err.message || message);
+  res.redirect('/');
 });
 
 /**
  * Render home.jade
- * @param  {http.IncomingMessage} req
- * @param  {http.ServerResponse} res
+ * @param  {IncomingMessage} req
+ * @param  {ServerResponse} res
  */
 router.get('/', function(req, res) {
-  res.render("home", { user: req.user });
+  res.render('home', { user: req.user });
+});
+
+/**
+ * Render layout for all other routes
+ * @param  {IncomingMessage} req
+ * @param  {ServerResponse} res
+ */
+router.get('*', function(req, res) {
+  res.render('layout', { user: req.user });
 });
 
 module.exports = router;
