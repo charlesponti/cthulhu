e2b87721ab82890202220dc9be1f1baefe9902f7
CJ Ponti <cjponti@gmail.com>
2014-12-04 14:37:41 +0000

    Add config param to logger.js function

--- src/logger.js
+++ src/logger.js
@@ -7,11 +7,16 @@ var winston = require('winston');
 /**
  * Configure logger
  * @param  {string} logfile Path to logfile
+ * @param  {object} config Configuration for transports
  * @return {winston.Logger}
  */
-module.exports = function(logfile) {
+module.exports = function(logfile, config) {
 
   var logFile = path.resolve(process.cwd(), logfile);
+
+  config = config || {};
+  config.file = config.file || {};
+  config.console = config.console || {};
 
   // If log file does not exist, create one.
   if (!fs.existsSync(logFile)) {
@@ -22,21 +27,21 @@ module.exports = function(logfile) {
 
   var logger = new winston.Logger({
     transports: [
-    new winston.transports.File({
-      level: 'info',
-      filename: logFile,
-      handleExceptions: true,
-      json: true,
-      maxsize: 5242880, //5MB
-      maxFiles: 5,
-      colorize: false
-    }),
-    new winston.transports.Console({
-      level: 'debug',
-      handleExceptions: true,
-      json: false,
-      colorize: true
-    })
+      new winston.transports.File({
+        level: config.file.level || 'info',
+        filename: logFile,
+        handleExceptions: config.file.handleExceptions || true,
+        json: config.file.json || true,
+        maxsize: config.file.maxsize || 5242880, // 5MB
+        maxFiles: config.file.level || 5,
+        colorize: config.file.level || false
+      }),
+      new winston.transports.Console({
+        level: config.console.level || 'debug',
+        handleExceptions: config.true,
+        json: config.json || false,
+        colorize: config.colorize || true
+      })
     ],
     exitOnError: false
   });
