691ed9eaf4507d3781109117f1bad19e1bf1888a
CJ Ponti <cjponti@gmail.com>
2014-12-05 11:29:08 +0000

    Finish remaining tests in middleware-spec.js

--- test/middleware-spec.js
+++ test/middleware-spec.js
@@ -41,11 +41,36 @@ describe('Cthulhu Middleware', function() {
   });
 
   describe('.locals()', function() {
-
+    it('should add values to res.locals', function() {
+      request.user = 'foo';
+      response.locals = {};
+      middleware.locals({ foo: 'bar' }, request, response, next);
+      expect(response.locals.foo).toEqual('bar');
+      expect(response.locals.app_name).toEqual('Cthulhu');
+      expect(response.locals.success_message).toEqual({});
+      expect(response.locals.error_message).toEqual({});
+      expect(response.locals.current_user).toEqual('foo');
+      expect(next).toHaveBeenCalled();
+    });
   });
 
   describe('.cors()', function() {
-
+    it('should set headers', function() {
+      middleware.cors(request, response, next);
+      expect(response._headers['access-control-allow-origin']).toEqual('*');
+      expect(response._headers['access-control-allow-methods'])
+        .toEqual('GET,PUT,POST,PATCH,DELETE');
+      expect(response._headers['access-control-allow-headers'])
+        .toEqual('X-Requested-With, X-Access-Token, X-Revision, Content-Type');
+      expect(next).toHaveBeenCalled();
+    });
+    it('should call res.send if OPTIONS request', function() {
+      spyOn(response, 'send');
+      request.method = 'OPTIONS';
+      middleware.cors(request, response, next);
+      expect(response.send).toHaveBeenCalledWith(200);
+      expect(next).not.toHaveBeenCalled();
+    });
   });
 
 });
