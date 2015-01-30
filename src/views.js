0a5097cac3aaddb7353c17b03c9c923e38eabed6
CJ Ponti <cjponti@gmail.com>
2015-01-30 10:31:58 +0000

    Create views.js for views settings

---
+++ src/views.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var path = require('path')
+var swig = require('swig')
+
+module.exports = function(cthulhu, viewsDir) {
+
+  // Set directory where views are stored.
+  if (viewsDir) {
+    cthulhu.set('views', path.resolve(global._cwd, viewsDir));
+  }
+
+  // Set view engine
+  cthulhu.engine('html', swig.renderFile);
+  cthulhu.set('view engine', 'html');
+
+  // Disable view caching if in development
+  if (global._env === 'development') {
+    cthulhu.set('view cache', false);
+    swig.setDefaults({
+      cache: false,
+      autoescape: false
+    });
+  } else {
+    swig.setDefaults({
+      autoescape: false
+    });
+  }
+
+  return;
+}
